a2958178d77ad87db7a98a158c014ea3188c8d68
= <navneil2006@gmail.com>
2020-06-16 21:38:57 +1000

    Capped Frame Rate and Added Controls

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 #include <map>
 using namespace std;
 
+const Uint8* keyboardState = SDL_GetKeyboardState(0);
+typedef struct {
+	float x, y;
+} Vector2f;
+
+bool isKeyPressed(SDL_Scancode key);
 int main(int argc, char* argv[]) {
 	const unsigned int SCREEN_WIDTH = 640;
 	const unsigned int SCREEN_HEIGHT = 480;
@@ -16,10 +22,43 @@ int main(int argc, char* argv[]) {
 	bool running = true;
 	SDL_Event event;
 	
+	Vector2f playerVelocity;
+	SDL_Rect player = {10,10,25,25};
+	playerVelocity.x=playerVelocity.y=0;
+	const float playerSpeed = 2.5f;
+	
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+	
+	Uint32 frameStart, frameTime;
+	
 	while (running) {
+		frameStart = SDL_GetTicks();
+		
 		SDL_PollEvent(&event);
 		if (event.type == SDL_QUIT)
 			running = false;
+		
+		if (isKeyPressed(SDL_SCANCODE_A))
+			playerVelocity.x = -playerSpeed;
+		else if (isKeyPressed(SDL_SCANCODE_D))
+			playerVelocity.x = playerSpeed;
+		
+		player.x += playerVelocity.x;
+		
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &player);
+		
+		SDL_RenderPresent(renderer);
+		
+		playerVelocity.x = playerVelocity.y = 0;
+		
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime)
+			SDL_Delay(frameDelay-frameTime);
 	}
 	
 	SDL_DestroyWindow(window);
@@ -27,4 +66,11 @@ int main(int argc, char* argv[]) {
 	SDL_Quit();
 	
 	return 0;
+}
+
+bool isKeyPressed(SDL_Scancode key) {
+	if (keyboardState)
+		if (keyboardState[key])
+			return true;
+	return false;
 }
